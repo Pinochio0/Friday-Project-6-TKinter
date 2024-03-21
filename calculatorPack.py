23c4aa05723e5f599b6d6bbb94b9f5a5bf22af1c
Skyler Emery <2get2skyler@gmail.com>
2024-03-21 18:40:03 -0500

    start of text and buttons

--- calculatorPack.py
+++ calculatorPack.py
@@ -1,1 +1,16 @@
 #Calculator code for tkinter using pack geometry
+
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+#defines window size
+root.geometry("500x500+10+10")
+
+textbox = Entry(root)
+textbox.pack(side = TOP)
+
+
+
+root.mainloop()
