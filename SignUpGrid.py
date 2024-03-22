9bce9a6ca4b34ee7b8219a1badb760d2cdc53975
Skyler Emery <2get2skyler@gmail.com>
2024-03-22 11:19:18 -0500

    final changes with text and window settings

--- SignUpGrid.py
+++ SignUpGrid.py
@@ -5,21 +5,26 @@ from tkinter import ttk
 
 root = Tk()
 
-frame = ttk.LabelFrame(root)
-frame.grid(row = 0, column = 0)
-frame.grid_propagate(False)
-frame.config(height = 200, width = 400)
-frame.config(text = "Sign Up")
+root.title('Sign Up')
+root.geometry("350x100+10+10")
 
 
+name_label = ttk.Label(root, text="Name:")
+name_label.grid(column=0, row=0)
 
-textboxName = ttk.Entry(frame,text ='Name')
-textboxName.grid(row=0,column=0)
+textboxName = ttk.Entry(root)
+textboxName.grid(row=0,column=1)
 
-textboxEmail = ttk.Entry(frame,text ='Email')
-textboxEmail.grid(row=1,column=0)
+email_label = ttk.Label(root, text="Email:")
+email_label.grid(column=0, row=1)
 
-textboxPassword = ttk.Entry(frame,text ='Password')
-textboxPassword.grid(row=2,column=0)
+textboxEmail = ttk.Entry(root)
+textboxEmail.grid(row=1,column=1)
+
+password_label = ttk.Label(root, text="Password:")
+password_label.grid(column=0, row=2)
+
+textboxPassword = ttk.Entry(root,text ='Password')
+textboxPassword.grid(row=2,column=1)
 
 root.mainloop()
